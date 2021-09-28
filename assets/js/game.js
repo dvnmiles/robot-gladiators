e1f78a388419a5ba984c7a90ccb0f30625fc13b1
DMM <dvnmls@gmail.com>
2021-09-27 20:42:47 -0700

    2n bug fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,6 +7,38 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+var fightOrSkip = function() {
+  // ask player if they'd like to fight or skip using fightOrSkip function
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+  // Enter the conditional recursive function call here!
+
+  // if player picks "skip" confirm and then stop the loop
+  if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerInfo.playerMoney = playerInfo.money - 10;
+      shop();
+    }
+  }
+}
+
+// Conditional Recursive Function Call
+if (promptFight === "" || promptFight === null) {
+  window.alert("You need to provide a valid answer! Please try again.");
+  return fightOrSkip();
+}
+
+if (!promptFight) {
+  window.alert("You need to provide a valid answer! Please try again.");
+  return fightOrSkip();
+ }
+
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
   while (playerInfo.health > 0 && enemy.health > 0) {
@@ -196,6 +228,7 @@ var shop = function() {
 // player information
 var playerInfo = {
   name: getPlayerName(),
+}
 
   var getPlayerName = function() {
     var name = "";
@@ -208,7 +241,9 @@ var playerInfo = {
     return name;
   };
   name: window.prompt("What is your robot's name?"),
-  
+  promptFight = promptFight.toLowerCase();
+
+if (promptFight === "skip") {
   health: 100,
   attack: 10,
   money: 10,
@@ -222,11 +257,12 @@ var playerInfo = {
       window.alert("Refilling player's health by 20 for 7 dollars.");
       this.health += 20;
       this.money -= 7;
-    } 
+    }
+
     else {
       window.alert("You don't have enough money!");
     }
-  },
+  }
   upgradeAttack: function() {
     if (this.money >= 7) {
       window.alert("Upgrading player's attack by 6 for 7 dollars.");
